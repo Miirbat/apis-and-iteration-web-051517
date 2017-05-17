f070444de4dfbbff9e11f524b3379e06e840a0bb
github username <github email address>
2017-05-17 18:57:51 -0400

    made sure the input is a star wars character

--- bin/run.rb
+++ bin/run.rb
@@ -3,10 +3,16 @@
 require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
 
-welcome
-character = get_character_from_user
-if individual_character(character) == "No"
-  puts "Not a real character"
-else
-show_character_movies(character)
+
+def star_wars_api
+  welcome
+  character = get_character_from_user
+  if individual_character(character) == "No"
+    puts "Not a real character"
+    star_wars_api
+  else
+    show_character_movies(character)
+  end
 end
+
+star_wars_api
